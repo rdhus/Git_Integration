402e54b07cc9640f820b24a8b688a4fbb0438a34
Rahul Dhus <70313866+rdhus@users.noreply.github.com>
2020-10-28 20:23:04 +0530

    Create Hello_world.java

---
+++ Hello_world.java
@@ -0,0 +1,7 @@
+# Simple Hello_world file to check git- Jenkins Integration
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
